1f1be28890c860fef073df2ad02213a9fea7bc71
Siraj <sirajkhanc2305@gmail.com>
2021-11-03 23:28:34 +0530

    Added Theme Switcher

--- style/script.js
+++ style/script.js
@@ -26,3 +26,41 @@ function rotate() {
         document.getElementById('mobile').classList.add('slide-one');
     }
 }
+let count1 = 1;
+function dark() {
+    count1++;
+    if (count1 % 2 == 0)
+    {
+        document.getElementById('body').classList.remove('body');
+        document.getElementById('body').classList.add('body-dark');
+        // document.getElementById('navbar').classList.remove('navbar');
+        document.getElementById('navbar').classList.add('navbar-dark');
+        document.getElementById('navbar-mobile').classList.add('navbar-dark');
+        document.getElementById('dark').classList.add('white');
+        document.getElementById('dark').innerText = 'Light Theme';
+        document.getElementById('dark-m').innerText = 'Light Theme';
+        let i = 0;
+        for (i = 0; i < 10; i++){
+            document.getElementsByClassName('h4')[i].classList.add('white');
+            document.getElementsByClassName('h3')[i].classList.add('white');
+        }
+    }
+    else{
+        document.getElementById('body').classList.add('body');
+        document.getElementById('body').classList.remove('body-dark');
+        // document.getElementById('navbar').classList.add('navbar');
+        document.getElementById('navbar').classList.remove('navbar-dark');
+        document.getElementById('navbar-mobile').classList.remove('navbar-dark');
+        document.getElementById('dark').classList.remove('white');
+        document.getElementById('dark').innerText = 'Dark Theme';
+        document.getElementById('dark-m').innerText = 'Dark Theme';
+        for (i = 0; i < 10; i++){
+            document.getElementsByClassName('h4')[i].classList.remove('white');
+            document.getElementsByClassName('h3')[i].classList.remove('white');
+        }
+    }
+}
+function mobile(){
+    rotate();
+    dark();
+}
